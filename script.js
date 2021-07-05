89598b00340db25e389ffd6b2550e227cb89fdf5
hardmid <hardmid@gmail.com>
2021-07-06 00:41:02 +0300

    lesson 1 fin

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
+    { title: 'Шорты', color: 'Красные', price: 150 }, // добавим цвет и переименуем данные
+    { title: 'Носки', color: 'Черные', price: 50 },
+    { title: 'Жакет', color: 'Желтый', price: 350 },
+    { title: 'Кроссы', color: 'Белые', price: 250 },
 ];
 
 
-const $goodsList = document.querySelector('.goods-list');
+const $goodsList = document.querySelector('.goods-list'); // определили блок
 
-const renderGoodsItem = ({ title, price }) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+const renderGoodsItem = ({ title, color, price }) => { // определили данные в функции вывода
+    return `<div class="goods-item"><h3>${title}</h3><p>${color} за ${price} биткоинов</p></div>`; // сформировали вывод
 };
 
-const renderGoodsList = (list = goods) => {
+const renderGoodsList = (list = goods) => { // рисуем данные
     let goodsList = list.map(
-            item => renderGoodsItem(item)
-        );
+            item => renderGoodsItem(item) // map конвертит данные в массиве, перечисление будет через запятую.
+        ).join('\n'); // добавляем сепаратор, разделитель для элементов массива
 
-    $goodsList.insertAdjacentHTML('beforeend', goodsList);
+    $goodsList.insertAdjacentHTML('beforeend', goodsList); // рисуем массив перед закрывающим тегом
 }
 
-renderGoodsList();
+renderGoodsList(); // выполняем
