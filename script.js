1e21b8dcf579f09e86c4ac4c9434ea5f84089ab5
hardmid <hardmid@gmail.com>
2021-07-06 21:52:09 +0300

    fix

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-const goods = [
-    { title: 'Шорты', color: 'Красные', price: 150 }, // добавим цвет и переименуем данные
-    { title: 'Носки', color: 'Черные', price: 50 },
-    { title: 'Жакет', color: 'Желтый', price: 350 },
-    { title: 'Кроссы', color: 'Белые', price: 250 },
-];
-
-
-const $goodsList = document.querySelector('.goods-list'); // определили блок
-
-const renderGoodsItem = ({ title, color, price }) => { // определили данные в функции вывода
-    return `<div class="goods-item"><h3>${title}</h3><p>${color} за ${price} биткоинов</p></div>`; // сформировали вывод
-};
-
-const renderGoodsList = (list = goods) => { // рисуем данные
-    let goodsList = list.map(
-            item => renderGoodsItem(item) // map конвертит данные в массиве, перечисление будет через запятую.
-        ).join('\n'); // добавляем сепаратор, разделитель для элементов массива
-
-    $goodsList.insertAdjacentHTML('beforeend', goodsList); // рисуем массив перед закрывающим тегом
-}
-
-// renderGoodsList(); // выполняем
-
-
 class Good {
     constructor(title, color, price) {
         this._title = title;
@@ -38,7 +13,6 @@ class Good {
         return `<div class="goods-item"><h3>${this._title}</h3><p>${this._color} по ${this._price} уевро</p></div>`;
     }
 }
-
 
 class GoodList {
     constructor (goods) {
